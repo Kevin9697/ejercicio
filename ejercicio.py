79858e0f267c4cb72446bf567675872cc683919e
Kevin9697 <134076960+Kevin9697@users.noreply.github.com>
2023-05-19 16:28:37 +0200

    Add files via upload

---
+++ ejercicio.py
@@ -0,0 +1,19 @@
+def sumar(n1, n2, n3):
+	suma = n1 + n2 + n3
+	return suma
+
+sumar(3, 5, 8)
+
+
+class coche:
+	numeropuertas = 4
+
+	def sumarpuertas():
+		numeropuertas += 1
+
+def sumar_puerta():
+	miCoche = coche()
+	miCoche.sumarpuertas()
+
+sumar_puerta()
+
